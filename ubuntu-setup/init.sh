c949c4109ac2f2e8d82e4692e5e5334a6cd807d0
gcdias <gustavo.carvalhodias@gmail.com>
2026-07-27 15:13:24 +0100

    init sh

---
+++ ubuntu-setup/init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#set -x
+function removedups(){
+    local d=""
+    local out=""
+    while [ $1 ]; do
+        if [[ -z "$(echo $out | grep $1)" ]]; then
+            out+=" $1"
+        fi
+        shift
+    done
+    echo ${out}
+}
+
+a=($(sudo cat /boot/grub/grub.cfg | grep 'menuentry' | grep -oE '\-\-class [a-zA-Z_\-]+' | sed 's,\-\-class ,,g'))
+grubEntries=($(removedups ${a[@]} | sed 's,os\|driver\|gnu-linux,,g'))
+grubEntries=$(printf '%s,' "${grubEntries[@]}" | sed 's/,$//')
+
+efiModes=($(xrandr -q | awk '{print $1}' | grep -oP '\d+x\d+'))
+res=($(sed 's,x, ,g' <<<"${efiModes[@]}"));
+efiModes=$(printf '%s,' "${efiModes[@]}" | sed 's/,$//')
+
+osv="$(lsb_release -rs)"
+osd="$(lsb_release -ds)"
+os="$(echo $osd | awk '{print $1}')"
+gnome="$(gnome-shell --version | grep -oP '\d\d')"
+
+vnd="$(sed 's/.*/\L&/' /sys/devices/virtual/dmi/id/sys_vendor)"
+case "${vnd,,}" in
+  *besstar*)   vnd="minisforum";;
+  *microsoft*) vnd="$(cat /sys/devices/virtual/dmi/id/product_family)";;
+esac
+
+microsoft-edge "https://gcdias.github.io/ubuntu-setup/index.html?usr=${USER}&vnd=${vnd}&w=${res[0]}&h=${res[1]}&m=${efiModes}&osv=${osv}&osd=${osd}&os=${os}"
+
+
+#set +x
---
+++ ubuntu-setup/main.js
@@ -0,0 +1,56 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const opts = {
+  vndList: ["acer", "apple", "asus", "beelink", "corsair", "dell", "fujitsu", "geekom", "gigabyte", "google", "hp", "intel", "lenovo", "lg", "microsoft", "minisforum", "msi", "razer", "samsung", "surface", "toshiba", "vaio", "xiaomi"],
+  osList: ["4MLinux", "AlpineLinux", "SystemRescueCD", "android", "anonymous", "antergos", "archlinux", "arcolinux", "artix", "blissos", "cancel", "centos", "chakra", "debian", "deepin", "devuan", "driver", "edit", "efi", "elementary", "endeavouros", "fedora", "freebsd", "gameros", "gentoo", "gnu-linux", "gpart", "haiku", "help", "kali", "kaos", "kbd", "korora", "kubuntu", "lang", "linuxmint", "lubuntu", "macosx", "mageia", "manjaro", "memtest", "mx-linux", "neon", "opensuse", "parrot", "peppermint", "pop-os", "puppy", "q4os", "recovery", "red", "regolith", "restart", "rocky", "shutdown", "siduction", "slackware", "solus", "sparky", "steamos", "submenu", "type", "tz", "ubuntu", "ubuntubudgie", "ubuntumate", "ubuntustudio", "uefi-firmware", "unset", "void", "windows", "windows10", "xubuntu", "zorin"],
+}
+
+const ui = {
+  vendor: document.getElementById('vendor'),
+  res: document.getElementById('res'),
+  os: document.getElementById('os'),
+  osv: document.getElementById('osv'),
+  init: function(){
+    this.vendor.replaceChildren();
+    this.os.replaceChildren();
+    
+    opts.osList.forEach((os) => ui.addOption(ui.os,os,os));
+    opts.vndList.forEach((vnd) => ui.addOption(ui.vendor,vnd,vnd));
+    let modes = urlParams.get("m");
+    if (modes){
+      this.res.replaceChildren();
+      modes.split(",")?.forEach(m => ui.addOption(ui.res,m,m));
+    }
+  },
+  addOption: function(parent, val, txt){
+    parent.appendChild(this.createOption(val, txt));
+  },
+  createOption: function(val, txt){
+    const o = document.createElement('option');
+    o.value = val;
+    o.textContent = txt;
+    return o;
+  },
+}
+
+
+
+const utils = {
+  addOption: function(parent, val, txt){
+    parent.appendChild(this.createOption(val, txt));
+  },
+  createOption: function(val, txt){
+    const o = document.createElement('option');
+    o.value = val;
+    o.textContent = txt;
+    return o;
+  },
+  downloadBlob: function(blob, filename) {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+  }
+}
