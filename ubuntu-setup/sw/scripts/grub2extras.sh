509cb5dd1ab7cd5ef9ffeaeef88ac83e6fa60ef8
diasgc <gustavocdias@me.com>
2026-08-19 18:23:02 +0100

    upg

--- ubuntu-setup/sw/scripts/grub2extras.sh
+++ ubuntu-setup/sw/scripts/grub2extras.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# 1. add reboot and shutdown menu entries to grub2 custom file
 a=($(sudo ls /etc/grub.d/*_custom))
 f=
 for i in "${a[@]}"; do
@@ -17,6 +19,7 @@ menuentry 'Reboot' --class restart --class os {
 	reboot
 }
 EOF
+echo "Added reboot menu entry to $f"
 fi
 
 if [[ -z "$(grep '\-\-class shutdown \-\-class os' "$f")" ]]; then
@@ -26,4 +29,16 @@ menuentry 'Shutdown' --class shutdown --class os {
 	halt
 }
 EOF
+echo "Added shutdown menu entry to $f"
 fi
+
+# 2. Add grub2 drive icon to "Advanced options for..." menu entry
+for i in $(find /etc/grub.d -name '*os-prober'); do
+	case $i in *backup*);;
+		*)
+			a='"Advanced options for %s"'
+			b='"${OS} $onstr"'
+			sudo sed -i "s/$a $b/$a \-\-class driver $b/g" $i && echo "Added grub2 drive icon to $i"
+		;;
+	esac;
+done
